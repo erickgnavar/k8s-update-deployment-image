9ce6c4b9e42060672281cdcb90c5a7dcf0cc5374
Erick Navarro <ericknavarro@resuelve.mx>
2021-10-06 12:39:47 -0500

    Initial commit, just testing

---
+++ Dockerfile
@@ -0,0 +1,16 @@
+FROM alpine:3.10
+
+RUN apk --update add curl
+
+# Download kubectl binary
+RUN curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+
+RUN chmod +x kubectl
+
+RUN mkdir -p ~/.local/bin/kubectl
+
+RUN mv ./kubectl ~/.local/bin/kubectl
+
+COPY entrypoint.sh /entrypoint.sh
+
+ENTRYPOINT ["/entrypoint.sh"]
---
+++ entrypoint.sh
@@ -0,0 +1,30 @@
+#!/bin/sh -l
+
+KUBERNETES_CERT="$1"
+KUBERNETES_SERVER="$2"
+KUBERNETES_TOKEN="$4"
+NAMESPACE="$3"
+DEPLOYMENT="$6"
+IMAGE="$7"
+CONTAINER="$7"
+
+env
+
+if [ -z "${KUBERNETES_USER}" ]; then
+  KUBERNETES_USER="default"
+fi
+
+kubectl config set-credentials default --token="${KUBERNETES_TOKEN}"
+
+echo "${KUBERNETES_CERT}" | base64 -d > ca.crt
+
+kubectl config set-cluster default --server="${KUBERNETES_SERVER}" --certificate-authority=ca.crt
+kubectl config use-context default
+
+IFS=',' read -r -a CONTAINERS <<< "${CONTAINER}"
+
+echo "Deploying to $KUBERNETES_SERVER"
+
+for CONTAINER in "${CONTAINERS[@]}"; do
+    kubectl -n "${NAMESPACE}" set image "deployment/${DEPLOYMENT}" "${CONTAINER}=${IMAGE}" --record
+done
