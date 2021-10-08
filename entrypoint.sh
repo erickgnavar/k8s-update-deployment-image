2e2c6eb2d5c443b8bcd19ad0eff2c8d361de1cd3
Erick Navarro <ericknavarro@resuelve.mx>
2021-10-08 10:30:13 -0500

    x12

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,24 +3,23 @@
 KUBERNETES_CERT="${INPUT_CERT}"
 KUBERNETES_SERVER="${INPUT_SERVER}"
 KUBERNETES_TOKEN="${INPUT_TOKEN}"
+KUBERNETES_USER="default"
+CONTEXT="default"
+CLUSTER_NAME="default"
 NAMESPACE="${INPUT_NAMESPACE}"
 DEPLOYMENT="${INPUT_NAME}"
 IMAGE="${INPUT_IMAGE}"
 CONTAINER="${INPUT_CONTAINER}"
 
-# TODO: remove this env call
-env
-
-if [ -z "${KUBERNETES_USER}" ]; then
-  KUBERNETES_USER="default"
-fi
-
-kubectl config set-credentials default --token="${KUBERNETES_TOKEN}"
-
 echo "${KUBERNETES_CERT}" | base64 -d > ca.crt
 
-kubectl config set-cluster default --server="${KUBERNETES_SERVER}" --certificate-authority=ca.crt
-kubectl config use-context default
+kubectl config set-cluster "${CLUSTER_NAME}" --server="${KUBERNETES_SERVER}" --certificate-authority=ca.crt
+
+kubectl config set-context "${CONTEXT}" --cluster="${CLUSTER_NAME}" --user="${KUBERNETES_USER}"
+
+kubectl config use-context "${CONTEXT}"
+
+kubectl config set-credentials "$CONTEXT" --token="$KUBERNETES_TOKEN"
 
 echo "Deploying to ${KUBERNETES_SERVER}"
 
